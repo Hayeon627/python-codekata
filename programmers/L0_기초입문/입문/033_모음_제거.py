253ac21bc553b854002441027f59cae8f89a78c6
Hayeon627 <hy2000627@gmail.com>
2026-02-10 09:41:06 +0900

    [프로그래머스] L0 - 모음 제거 (#33)

---
+++ programmers/L0_기초입문/입문/033_모음_제거.py
@@ -0,0 +1,12 @@
+# 모음 제거
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120849
+# 알고리즘: 기초
+# 작성자: 김하연
+# 작성일: 2026. 02. 10. 09:41:05
+
+def solution(my_string):
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    for v in vowels:
+        my_string = my_string.replace(v, '')
+    return my_string
