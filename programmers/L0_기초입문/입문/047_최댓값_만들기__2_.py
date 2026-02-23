a8a6af9259a17195ab4971cad90d49fe17f4a060
Hayeon627 <hy2000627@gmail.com>
2026-02-23 09:42:45 +0900

    [프로그래머스] L0 - 최댓값 만들기 (2) (#47)

--- programmers/L0_기초입문/입문/047_최댓값_만들기__2_.py
+++ programmers/L0_기초입문/입문/047_최댓값_만들기__2_.py
@@ -3,23 +3,22 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120862
 # 알고리즘: 기초
 # 작성자: 김하연
-# 작성일: 2026. 02. 23. 09:41:24
-
-# def solution(numbers):
-#     answer = 0
-#     for i in numbers:
-#         if i > answer:
-#             answer 
-#     return answer
+# 작성일: 2026. 02. 23. 09:42:45
 
 def solution(numbers):
-    answer = numbers[0] * numbers[1]
-    n = len(numbers)
+    numbers.sort()
+    case1 = numbers[0] * numbers[1]
+    case2 = numbers[-1] * numbers[-2]
+    return max(case1, case2)
+
+# def solution(numbers):
+#     answer = numbers[0] * numbers[1]
+#     n = len(numbers)
     
-    for i in range(n):
-        for j in range(i + 1, n):
-            product = numbers[i] * numbers[j]
-            if product > answer:
-                answer = product
+#     for i in range(n):
+#         for j in range(i + 1, n):
+#             product = numbers[i] * numbers[j]
+#             if product > answer:
+#                 answer = product
                 
-    return answer
+#     return answer
