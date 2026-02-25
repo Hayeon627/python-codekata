73a714baaa513819be0cc5bb311489372b53052f
Hayeon627 <hy2000627@gmail.com>
2026-02-25 16:29:44 +0900

    [프로그래머스] L0 - 가장 큰 수 찾기 (#51)

--- programmers/L0_기초입문/입문/051_가장_큰_수_찾기.py
+++ programmers/L0_기초입문/입문/051_가장_큰_수_찾기.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120899
 # 알고리즘: 기초
 # 작성자: 김하연
-# 작성일: 2026. 02. 25. 16:27:38
+# 작성일: 2026. 02. 25. 16:29:43
 
 def solution(array):
     n_max = 0
@@ -14,3 +14,14 @@ def solution(array):
             i_max = i
     answer = [n_max, i_max]
     return answer
+
+# 내장함수
+def solution(array):
+    n_max = max(array)
+    i_max = array.index(n_max)
+    return [n_max, i_max]
+
+# max + enumerate
+def solution(array):
+    n_max, i_max = max((v, i) for i, v in enumerate(array))
+    return [n_max, i_max]
