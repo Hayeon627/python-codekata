23e96343bc559aa95d5aaba3d37154a25613246f
Hayeon627 <hy2000627@gmail.com>
2026-02-26 09:45:50 +0900

    [프로그래머스] L0 - 대문자와 소문자 (#54)

--- programmers/L0_기초입문/입문/054_대문자와_소문자.py
+++ programmers/L0_기초입문/입문/054_대문자와_소문자.py
@@ -3,16 +3,16 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120893
 # 알고리즘: 기초
 # 작성자: 김하연
-# 작성일: 2026. 02. 26. 09:45:42
+# 작성일: 2026. 02. 26. 09:45:49
 
-# def solution(my_string):
-#     answer = ''
-#     for a in my_string:
-#         if a.islower():
-#             answer += a.upper()
-#         else:
-#             answer += a.lower()
-#     return answer
+def solution(my_string):
+    answer = ''
+    for a in my_string:
+        if a.islower():
+            answer += a.upper()
+        else:
+            answer += a.lower()
+    return answer
 
 def solution(my_string):
     answer = my_string.swapcase()
