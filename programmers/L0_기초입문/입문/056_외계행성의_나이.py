9275cb26af981a22d65cb74181afd5873ec1d73d
Hayeon627 <hy2000627@gmail.com>
2026-03-12 16:52:43 +0900

    [프로그래머스] L0 - 외계행성의 나이 (#56)

--- programmers/L0_기초입문/입문/056_외계행성의_나이.py
+++ programmers/L0_기초입문/입문/056_외계행성의_나이.py
@@ -3,10 +3,13 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120834
 # 알고리즘: 기초
 # 작성자: 김하연
-# 작성일: 2026. 03. 12. 16:52:16
+# 작성일: 2026. 03. 12. 16:52:42
 
 def solution(age):
     answer = ''
     for a in str(age):
         answer += chr(int(a) + ord('a'))
     return answer
+
+def solution(age):
+    return "".join([chr(int(a) + ord('a')) for a in str(age)])
