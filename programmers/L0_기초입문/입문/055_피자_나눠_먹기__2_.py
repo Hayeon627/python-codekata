00248d107fc67d55ccafaa9a72dffeb8ea93239c
Hayeon627 <hy2000627@gmail.com>
2026-03-12 16:42:54 +0900

    [프로그래머스] L0 - 피자 나눠 먹기 (2) (#55)

--- programmers/L0_기초입문/입문/055_피자_나눠_먹기__2_.py
+++ programmers/L0_기초입문/입문/055_피자_나눠_먹기__2_.py
@@ -3,10 +3,16 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120815
 # 알고리즘: 기초
 # 작성자: 김하연
-# 작성일: 2026. 03. 12. 16:42:28
+# 작성일: 2026. 03. 12. 16:42:53
 
 # 라이브러리 사용
-import math
+# import math
+
+# def solution(n):
+#     return (n * 6 // math.gcd(n, 6)) // 6
 
 def solution(n):
-    return (n * 6 // math.gcd(n, 6)) // 6
+    pizza = 1
+    while (pizza * 6) % n != 0:
+        pizza += 1
+    return pizza
