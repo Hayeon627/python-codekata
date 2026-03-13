c562b8348a68568d0792576f9936e9a21899cf95
Hayeon627 <hy2000627@gmail.com>
2026-03-13 09:51:02 +0900

    [프로그래머스] L0 - 배열 회전시키기 (#57)

--- programmers/L0_기초입문/입문/057_배열_회전시키기.py
+++ programmers/L0_기초입문/입문/057_배열_회전시키기.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120844
 # 알고리즘: 기초
 # 작성자: 김하연
-# 작성일: 2026. 03. 13. 09:49:06
+# 작성일: 2026. 03. 13. 09:51:02
 
 def solution(numbers, direction):
     answer = []
@@ -12,3 +12,6 @@ def solution(numbers, direction):
     else:
         answer = numbers[1:] + [numbers[0]]          
     return answer
+# [numbers[-1]] 대괄호로 묶는 이유 :
+# +를 사용하기 위해서는 데이터 타입이 같아야 함.
+# 똑같이 리스트 형태로 만들어준 뒤에 합칠 수 있음
