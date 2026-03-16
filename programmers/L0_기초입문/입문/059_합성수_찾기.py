9c4b5f633969b656a65f353608e5278c2e91189b
Hayeon627 <hy2000627@gmail.com>
2026-03-16 18:02:02 +0900

    [프로그래머스] L0 - 합성수 찾기 (#59)

--- programmers/L0_기초입문/입문/059_합성수_찾기.py
+++ programmers/L0_기초입문/입문/059_합성수_찾기.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120846
 # 알고리즘: 기초
 # 작성자: 김하연
-# 작성일: 2026. 03. 16. 18:01:15
+# 작성일: 2026. 03. 16. 18:02:02
 
 def solution(n):
     answer = []
@@ -15,3 +15,14 @@ def solution(n):
         if count >= 3:
             answer.append(i)
     return len(answer)
+
+import math
+
+def solution(n):
+    answer = 0
+    for i in range(4, n + 1): # 1, 2, 3은 합성수가 아니므로 4부터 시작
+        for j in range(2, int(math.sqrt(i)) + 1):
+            if i % j == 0:
+                answer += 1
+                break # 합성수임을 확인했으므로 더 이상 나눌 필요 없음
+    return answer
