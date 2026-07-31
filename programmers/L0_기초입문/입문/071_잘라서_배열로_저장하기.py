10685cfc6f035510fdaabc7a75efe1cb4241724d
Hayeon627 <hy2000627@gmail.com>
2026-07-31 17:09:20 +0900

    [프로그래머스] L0 - 잘라서 배열로 저장하기 (#71)

---
+++ programmers/L0_기초입문/입문/071_잘라서_배열로_저장하기.py
@@ -0,0 +1,12 @@
+# 잘라서 배열로 저장하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120913
+# 알고리즘: 기초
+# 작성자: 김하연
+# 작성일: 2026. 07. 31. 17:09:18
+
+def solution(my_str, n):
+    answer = []
+    for i in range(0, len(my_str), n):
+        answer.append(my_str[i:i+n])
+    return answer
