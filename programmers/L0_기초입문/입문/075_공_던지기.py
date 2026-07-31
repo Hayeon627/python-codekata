a689087be5959dfc1047f12edf42e7f61af72730
Hayeon627 <hy2000627@gmail.com>
2026-07-31 17:51:20 +0900

    [프로그래머스] L0 - 공 던지기 (#75)

---
+++ programmers/L0_기초입문/입문/075_공_던지기.py
@@ -0,0 +1,11 @@
+# 공 던지기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120843
+# 알고리즘: 기초
+# 작성자: 김하연
+# 작성일: 2026. 07. 31. 17:51:18
+
+def solution(numbers, k):
+    index = 2 * (k-1)
+    answer = numbers[index % len(numbers)]
+    return answer
