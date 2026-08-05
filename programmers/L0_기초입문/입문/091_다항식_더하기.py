1ad2a1ffe89321722555f51c95e2c8c40b079df6
Hayeon627 <hy2000627@gmail.com>
2026-08-06 05:01:52 +0900

    [프로그래머스] L0 - 다항식 더하기 (#91)

--- programmers/L0_기초입문/입문/091_다항식_더하기.py
+++ programmers/L0_기초입문/입문/091_다항식_더하기.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120863
 # 알고리즘: 기초
 # 작성자: 김하연
-# 작성일: 2026. 08. 06. 05:01:05
+# 작성일: 2026. 08. 06. 05:01:51
 
 def solution(polynomial):
     answer = ''
@@ -34,3 +34,25 @@ def solution(polynomial):
         answer = str(n_sum)
 
     return answer
+
+
+# 더 효율적인 방법
+def solution(polynomial):
+    x_sum = 0
+    n_sum = 0
+    
+    # 1. 계산부 (공백 제거 후 '+' 기준으로 나누면 연산자 처리가 필요 없어짐)
+    for term in polynomial.replace(' ', '').split('+'):
+        if 'x' in term:
+            x_sum += 1 if term == 'x' else int(term[:-1])
+        else:
+            n_sum += int(term)
+            
+    # 2. 출력 조립부 (if-else 분기를 배열과 join으로 단일화)
+    result = []
+    if x_sum:
+        result.append("x" if x_sum == 1 else f"{x_sum}x")
+    if n_sum:
+        result.append(str(n_sum))
+        
+    return " + ".join(result) if result else "0"
