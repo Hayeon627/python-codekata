9f060aff998d1b013d6419487ac888eea0c7a93e
Hayeon627 <hy2000627@gmail.com>
2026-08-07 13:37:04 +0900

    [프로그래머스] L0 - 다음에 올 숫자 (#94)

---
+++ programmers/L0_기초입문/입문/094_다음에_올_숫자.py
@@ -0,0 +1,14 @@
+# 다음에 올 숫자
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120924
+# 알고리즘: 기초
+# 작성자: 김하연
+# 작성일: 2026. 08. 07. 13:37:03
+
+def solution(common):
+    answer = 0
+    if (common[1] - common[0]) == (common[2] - common[1]):
+        answer = common[1] - common[0] + common[-1]
+    else:
+        answer = common[1] // common[0] * common[-1]
+    return answer
